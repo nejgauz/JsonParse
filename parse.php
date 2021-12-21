7d82f2f900fa08fa6351e94d5e4a18d26d51e163
анна <shesterikofff@mail.ru>
2021-12-22 00:15:02 +0300

    recursive object parsing added

--- parse.php
+++ parse.php
@@ -1,82 +1,92 @@
 <?php
-$handle = fopen('simple.json', 'r');
 
+const NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const KEY_WRITING_PROCESS = "key_writing";
+const VALUE_WRITING_PROCESS = "value_writing";
+
+$handle = fopen('simple.json', 'r+');
 $object = new class {};
 
-function parseJsonString(&$object, $handle, $value = null)
+function parseJson(&$object, $handle)
 {
-    $isStartRecordProperty = false;
-    $isStartRecordValue = false;
-    $prepareRecordValue = false;
-
-    $property = '';
+    $previousSymbol = false;
+    $key = '';
+    $state = KEY_WRITING_PROCESS;
     while (false !== ($char = fgetc($handle))) {
-        if ($char === '}') {
-            $object->{$property} = $value;
-            return;
-        }
-        if ($isStartRecordProperty) {
-            if ('"' === $char) {
-                $isStartRecordProperty = false;
-                $object->{$property} = null;
-                continue;
-            } else {
-                $property .= $char;
-            }
-        }
-        if ('"' === $char && !$isStartRecordProperty && !$prepareRecordValue) {
-            $isStartRecordProperty = true;
+        if (($char === "}" || $char === "]") && $previousSymbol !== "\\") {
+            break;
         }
 
-        if ($isStartRecordValue) {
-            if (',' === $char) {
-                $isStartRecordValue = false;
-                $value = purify($value);
-                $object->{$property} = $value;
-                $property = '';
-                $value = null;
+        if ($char === "{" && $previousSymbol !== "\\" && $previousSymbol !== false) {
+            $object->{"$key"} = new class {};
+            parseJson($object->{"$key"}, $handle);
+            continue;
+        }
+
+        if ($state === KEY_WRITING_PROCESS) {
+
+            if ($char === "\"") {
+                $key = getStringValue($handle);
+                $object->{$key} = null;
+                $state = VALUE_WRITING_PROCESS;
                 continue;
-            } else {
-                if ('{' === $char) {
-                    parseJsonString($property, $handle,  new class {});
-                }
-                if ('[' === $char) {
-                    parseJsonString($property, $handle, []);
-                }
-                if (!$value && $char === ' ') {
-                    continue;
-                }
-                $value .= $char;
             }
         }
-        if (':' === $char && !$isStartRecordProperty && !$isStartRecordValue) {
-            $isStartRecordValue = true;
+
+        if ($state === VALUE_WRITING_PROCESS) {
+
+            switch ($char) {
+                case "\"":
+                    $value = getStringValue($handle);
+                    $object->{$key} = $value;
+                    $state = KEY_WRITING_PROCESS;
+                    break;
+                case "t":
+                    $object->{$key} = true;
+                    fseek($handle, 4, SEEK_CUR);
+                    $state = KEY_WRITING_PROCESS;
+                    break;
+                case "f":
+                    $object->{$key} = false;
+                    fseek($handle, 4, SEEK_CUR);
+                    $state = KEY_WRITING_PROCESS;
+                    break;
+            }
+
+            if (in_array($char, NUMBERS)) {
+                $number = getNumberValue($char, $handle);
+                $object->{$key} = $number;
+                $state = KEY_WRITING_PROCESS;
+            }
         }
+
+        $previousSymbol = $char;
     }
 }
 
-
-function purify(string $value): string
+function getNumberValue($firstChar, &$handle): int
 {
-    if ($value === 'true') {
-        $value = true;
+    $number = $firstChar;
+    while (($char = fgetc($handle)) !== "," && $char !== false) {
+        $number .= $char;
     }
-    if ($value === 'false') {
-        $value = false;
-    }
-    if (preg_match('/[0-9]+/', $value)) {
-        $value = (int)$value;
-    }
-    if (preg_replace('/^"/', $value) || preg_match(' ', $value)) {
-
-    }
-    if ($value[0] === '"' && mb_substr($value, -1) === '"') {
-        $value = mb_substr($value, 1, mb_strlen($value));
-    }
+    return intval($number);
 }
 
-parseJsonString($object, $handle);
+function getStringValue(&$handle): string
+{
+    $string = "";
+    $previousSymbol = "";
+    while (($char = fgetc($handle)) !== false && !($char === "\"" && $previousSymbol !== "\\")) {
+        $string .= $char;
+        $previousSymbol = $char;
+    }
+    fseek($handle, 1, SEEK_CUR);
+    return $string;
+}
 
-var_dump($object);
+parseJson($object, $handle);
+
+echo $object->address->street;
 
 fclose($handle);
